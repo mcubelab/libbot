f1d4a6777708d60f37c79f92ec7c9c792f16cb24
Rachel Holladay <rachel.holladay@gmail.com>
2017-06-10 12:49:10 -0400

    Sort output config

--- bot2-procman/python/src/bot_procman/sheriff_config.py
+++ bot2-procman/python/src/bot_procman/sheriff_config.py
@@ -203,12 +203,14 @@ class GroupNode(object):
         s = "    " * indent
         if self.name == "":
             assert indent == 0
+            sorted_cmd = sorted(self.commands, key=lambda c: c.attributes['nickname'])
             val = "\n".join([group.to_config_string(0) for group in self.subgroups.values()])
-            val = val + "\n".join([cmd.to_config_string(0) for cmd in self.commands]) + "\n"
+            val = val + "\n".join([cmd.to_config_string(0) for cmd in sorted_cmd]) + "\n"
         else:
             val = "%sgroup \"%s\" {\n" % (s, self.name)
+            sorted_cmd = sorted(self.commands, key=lambda c: c.attributes['nickname'])
             val = val + "\n".join([group.to_config_string(indent+1) for group in self.subgroups.values()])
-            val = val + "\n".join([cmd.to_config_string(indent+1) for cmd in self.commands])
+            val = val + "\n".join([cmd.to_config_string(indent+1) for cmd in sorted_cmd])
             val = val + "\n%s}\n" % s
         return val
 
